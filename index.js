83c4e50a1e1709bd9b7753cfd985898c96f5fc85
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-08-31 00:03:10 +0530

    solving problem 1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello Bro ");
+Q . 1 .Write a program to check whether a number is even or odd.
+
+const  arr = [2,3,5,7,8,9 ,6]  ; 
+const result = arr.map(num=>{
+        if(num%2==0){
+            return "Even" ;       
+        }else{
+           return "Odd"
+        }
+})
+console.log(result);
